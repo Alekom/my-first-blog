f67b0edda526e4960ac6b2cb4a4f4ef152d4629c
Your Name <you@example.com>
2017-06-04 21:01:04 +0300

    Added counter for approved comments.

--- blog/models.py
+++ blog/models.py
@@ -3,20 +3,21 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+	
+	def approved_comments(self):
+		return self.comments.filter(approved_comment=True)
+	
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+	
+	def __str__(self):
+		return self.title
 
 class Comment(models.Model):
 	post = models.ForeignKey('blog.Post', related_name='comments')
@@ -31,6 +32,3 @@ class Comment(models.Model):
 
 	def __str__(self):
 		return self.text
-	
-	def approved_comments(self):
-		return self.comments.filter(approved_comment=True)
